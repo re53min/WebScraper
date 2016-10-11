6776950b5b75c85d7038e05ee98d3953ddf12d58
re53min <magicalpuyo2@gmail.com>
2016-10-11 22:49:55 +0900

    ファイル読み込みの廃止してレンジでループ。またHTMLから品名の取得。

--- webscrapy.py
+++ webscrapy.py
@@ -14,51 +14,34 @@ sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
 
 # Main
 def main():
+    # URLの雛形
+    url = "http://wiimk2.net/wiiu/title.php?title={number}"
+
     replace_dict = {
         # 漢字の表記ゆれ
-        u'(旨い|美味い|ウマい)': u'うまい',
-        u'美味しい': u'おいしい',
-        u'(万寿|万壽)': u'萬寿',
-        u'端麗': u'淡麗',
-        u'(cp|CP|Cp|cP)': u'コストパフォーマンス',
-        u'百壽': u'百寿',
-        u'千壽': u'千寿',
-        u'(うま味|うまみ|旨み)': u'旨味',
-        u'さわやか': u'爽やか',
-        u'吟': u'純米吟醸',
 
-        # 味わい表現
-        u'(フルーティー|フルーティ|甘い|甘み|優しい|やさしい|柔らかい|甘口)': u'甘味',
-        # u'(酸っぱい|酸|辛い|辛口|ドライ|キレ|からい|きれ|)': u'酸味', #出力が変になるためいったんコメントアウト
-        u'(まろやか|コク|穏やか|後味|濃い|旨|深い|滑らか)': u'旨味',
-        u'(苦い|苦味|雑|にがみ|えぐみ|邪魔|ひどい|くどい)': u'雑味',
     }
 
-    # URLリストから1行ずつ読み、対象のwebページを取得し、任意のタグのテキストをスクレイピング
-    for target_url in open('url.txt').readlines():
-        target_url = target_url.split(",")
-        sake_name = target_url[0].decode('utf-8')
-        res = request.urlopen(target_url[1])  # URLオープン
+    # title={number}を500までループ
+    for number in xrange(500):
+        res = request.urlopen(url.format(number=number+1))  # URLオープン
         soup = BeautifulSoup(res.read())  # 読み込み
 
-        # レビュー者を削除
-        for tmp in soup.findAll("div", {"class": "voicemember"}):
-            tmp.extract()
-        html_div = soup.find('div', {'id': 'maincontent'})  # メインコンテンツの取得
-        texts = html_div.findAll('dd')  # メインコンテンツ内のレビューのみをスクレイピング
+        title = soup.find('div', {'class': 'review'}).find('h2').string  # ゲームタイトルの取得
+        reviews = soup.find('div', {'class': 'user_review_comments'}).findAll('p')  # レビュー文の取得
 
         # スクレイピングしたレビュー処理
-        for text in texts:
-            text = text.text  # text形式(unicode)に変換
+        for review in reviews:
+            text = review.text  # text形式(unicode)に変換
             # normResult = normalizeText(text) #不要な空白・改行等の処理
 
             # 形態素解析処理
-            sake_wakati = mecab.extract_key_word(sake_name, text)
+            wakati_text = mecab.extract_key_word(title, text)
             # wordCount.fileOutput(strWakati)
 
             # リストの内容を標準出力
-            for w in sake_wakati:
-                print mecab.multiple_replace(w, replace_dict),
+            for w in wakati_text:
+                print w,  # mecab.multiple_replace(w, replace_dict),
             print ''
 
             time.sleep(2.0)  # 念のために2秒間のdelay
